1d6e67697207562843ddfde87a864fd4c5f2be4d
Ken You <saykent@gmail.com>
2023-03-01 14:19:12 +1100

    agent bot with api key

--- examples/agent-bot/main.js
+++ examples/agent-bot/main.js
@@ -10,11 +10,26 @@
 
 const MyCoolAgent = require('./MyCoolAgent');
 
-const conf = {
-    accountId: process.env.LP_ACCOUNT,
-    username: process.env.LP_USER,
-    password: process.env.LP_PASS
-};
+let conf_temp = {};
+
+if (process.env.LP_KEY) {
+    conf_temp = {
+        accountId: process.env.LP_ACCOUNT,
+        username: process.env.LP_USER,
+        appKey: process.env.LP_KEY,
+        secret: process.env.LP_SECRET,
+        accessToken: process.env.LP_TOKEN,
+        accessTokenSecret: process.env.LP_TOKEN_SECRET
+    };
+} else {
+    conf_temp = {
+        accountId: process.env.LP_ACCOUNT,
+        username: process.env.LP_USER,
+        password: process.env.LP_PASS
+    };
+}
+const conf = Object.assign({}, conf_temp);
+
 if (process.env.LP_CSDS) {
     conf.csdsDomain = process.env.LP_CSDS;
 }
